344728b9d0fbd77822cdbaf3fcc886358db7f1b4
Sebastián Salata R-T <sa.salatart@gmail.com>
2024-04-20 09:09:25 -0400

    Add base Answer model (#2)

--- db/model/choices.go
+++ db/model/choices.go
@@ -63,33 +63,6 @@ var ChoiceTableColumns = struct {
 
 // Generated where
 
-type whereHelperstring struct{ field string }
-
-func (w whereHelperstring) EQ(x string) qm.QueryMod     { return qmhelper.Where(w.field, qmhelper.EQ, x) }
-func (w whereHelperstring) NEQ(x string) qm.QueryMod    { return qmhelper.Where(w.field, qmhelper.NEQ, x) }
-func (w whereHelperstring) LT(x string) qm.QueryMod     { return qmhelper.Where(w.field, qmhelper.LT, x) }
-func (w whereHelperstring) LTE(x string) qm.QueryMod    { return qmhelper.Where(w.field, qmhelper.LTE, x) }
-func (w whereHelperstring) GT(x string) qm.QueryMod     { return qmhelper.Where(w.field, qmhelper.GT, x) }
-func (w whereHelperstring) GTE(x string) qm.QueryMod    { return qmhelper.Where(w.field, qmhelper.GTE, x) }
-func (w whereHelperstring) LIKE(x string) qm.QueryMod   { return qm.Where(w.field+" LIKE ?", x) }
-func (w whereHelperstring) NLIKE(x string) qm.QueryMod  { return qm.Where(w.field+" NOT LIKE ?", x) }
-func (w whereHelperstring) ILIKE(x string) qm.QueryMod  { return qm.Where(w.field+" ILIKE ?", x) }
-func (w whereHelperstring) NILIKE(x string) qm.QueryMod { return qm.Where(w.field+" NOT ILIKE ?", x) }
-func (w whereHelperstring) IN(slice []string) qm.QueryMod {
-	values := make([]interface{}, 0, len(slice))
-	for _, value := range slice {
-		values = append(values, value)
-	}
-	return qm.WhereIn(fmt.Sprintf("%s IN ?", w.field), values...)
-}
-func (w whereHelperstring) NIN(slice []string) qm.QueryMod {
-	values := make([]interface{}, 0, len(slice))
-	for _, value := range slice {
-		values = append(values, value)
-	}
-	return qm.WhereNotIn(fmt.Sprintf("%s NOT IN ?", w.field), values...)
-}
-
 type whereHelperbool struct{ field string }
 
 func (w whereHelperbool) EQ(x bool) qm.QueryMod  { return qmhelper.Where(w.field, qmhelper.EQ, x) }
@@ -98,27 +71,6 @@ func (w whereHelperbool) LT(x bool) qm.QueryMod  { return qmhelper.Where(w.field
 func (w whereHelperbool) LTE(x bool) qm.QueryMod { return qmhelper.Where(w.field, qmhelper.LTE, x) }
 func (w whereHelperbool) GT(x bool) qm.QueryMod  { return qmhelper.Where(w.field, qmhelper.GT, x) }
 func (w whereHelperbool) GTE(x bool) qm.QueryMod { return qmhelper.Where(w.field, qmhelper.GTE, x) }
-
-type whereHelpertime_Time struct{ field string }
-
-func (w whereHelpertime_Time) EQ(x time.Time) qm.QueryMod {
-	return qmhelper.Where(w.field, qmhelper.EQ, x)
-}
-func (w whereHelpertime_Time) NEQ(x time.Time) qm.QueryMod {
-	return qmhelper.Where(w.field, qmhelper.NEQ, x)
-}
-func (w whereHelpertime_Time) LT(x time.Time) qm.QueryMod {
-	return qmhelper.Where(w.field, qmhelper.LT, x)
-}
-func (w whereHelpertime_Time) LTE(x time.Time) qm.QueryMod {
-	return qmhelper.Where(w.field, qmhelper.LTE, x)
-}
-func (w whereHelpertime_Time) GT(x time.Time) qm.QueryMod {
-	return qmhelper.Where(w.field, qmhelper.GT, x)
-}
-func (w whereHelpertime_Time) GTE(x time.Time) qm.QueryMod {
-	return qmhelper.Where(w.field, qmhelper.GTE, x)
-}
 
 var ChoiceWhere = struct {
 	ID         whereHelperstring
@@ -137,13 +89,16 @@ var ChoiceWhere = struct {
 // ChoiceRels is where relationship names are stored.
 var ChoiceRels = struct {
 	Question string
+	Answers  string
 }{
 	Question: "Question",
+	Answers:  "Answers",
 }
 
 // choiceR is where relationships are stored.
 type choiceR struct {
-	Question *Question `boil:"Question" json:"Question" toml:"Question" yaml:"Question"`
+	Question *Question   `boil:"Question" json:"Question" toml:"Question" yaml:"Question"`
+	Answers  AnswerSlice `boil:"Answers" json:"Answers" toml:"Answers" yaml:"Answers"`
 }
 
 // NewStruct creates a new relationship struct
@@ -156,6 +111,13 @@ func (r *choiceR) GetQuestion() *Question {
 		return nil
 	}
 	return r.Question
+}
+
+func (r *choiceR) GetAnswers() AnswerSlice {
+	if r == nil {
+		return nil
+	}
+	return r.Answers
 }
 
 // choiceL is where Load methods for each relationship are stored.
@@ -485,6 +447,20 @@ func (o *Choice) Question(mods ...qm.QueryMod) questionQuery {
 	return Questions(queryMods...)
 }
 
+// Answers retrieves all the answer's Answers with an executor.
+func (o *Choice) Answers(mods ...qm.QueryMod) answerQuery {
+	var queryMods []qm.QueryMod
+	if len(mods) != 0 {
+		queryMods = append(queryMods, mods...)
+	}
+
+	queryMods = append(queryMods,
+		qm.Where("\"answers\".\"choice_id\"=?", o.ID),
+	)
+
+	return Answers(queryMods...)
+}
+
 // LoadQuestion allows an eager lookup of values, cached into the
 // loaded structs of the objects. This is for an N-1 relationship.
 func (choiceL) LoadQuestion(ctx context.Context, e boil.ContextExecutor, singular bool, maybeChoice interface{}, mods queries.Applicator) error {
@@ -605,6 +581,119 @@ func (choiceL) LoadQuestion(ctx context.Context, e boil.ContextExecutor, singula
 	return nil
 }
 
+// LoadAnswers allows an eager lookup of values, cached into the
+// loaded structs of the objects. This is for a 1-M or N-M relationship.
+func (choiceL) LoadAnswers(ctx context.Context, e boil.ContextExecutor, singular bool, maybeChoice interface{}, mods queries.Applicator) error {
+	var slice []*Choice
+	var object *Choice
+
+	if singular {
+		var ok bool
+		object, ok = maybeChoice.(*Choice)
+		if !ok {
+			object = new(Choice)
+			ok = queries.SetFromEmbeddedStruct(&object, &maybeChoice)
+			if !ok {
+				return errors.New(fmt.Sprintf("failed to set %T from embedded struct %T", object, maybeChoice))
+			}
+		}
+	} else {
+		s, ok := maybeChoice.(*[]*Choice)
+		if ok {
+			slice = *s
+		} else {
+			ok = queries.SetFromEmbeddedStruct(&slice, maybeChoice)
+			if !ok {
+				return errors.New(fmt.Sprintf("failed to set %T from embedded struct %T", slice, maybeChoice))
+			}
+		}
+	}
+
+	args := make(map[interface{}]struct{})
+	if singular {
+		if object.R == nil {
+			object.R = &choiceR{}
+		}
+		args[object.ID] = struct{}{}
+	} else {
+		for _, obj := range slice {
+			if obj.R == nil {
+				obj.R = &choiceR{}
+			}
+			args[obj.ID] = struct{}{}
+		}
+	}
+
+	if len(args) == 0 {
+		return nil
+	}
+
+	argsSlice := make([]interface{}, len(args))
+	i := 0
+	for arg := range args {
+		argsSlice[i] = arg
+		i++
+	}
+
+	query := NewQuery(
+		qm.From(`answers`),
+		qm.WhereIn(`answers.choice_id in ?`, argsSlice...),
+	)
+	if mods != nil {
+		mods.Apply(query)
+	}
+
+	results, err := query.QueryContext(ctx, e)
+	if err != nil {
+		return errors.Wrap(err, "failed to eager load answers")
+	}
+
+	var resultSlice []*Answer
+	if err = queries.Bind(results, &resultSlice); err != nil {
+		return errors.Wrap(err, "failed to bind eager loaded slice answers")
+	}
+
+	if err = results.Close(); err != nil {
+		return errors.Wrap(err, "failed to close results in eager load on answers")
+	}
+	if err = results.Err(); err != nil {
+		return errors.Wrap(err, "error occurred during iteration of eager loaded relations for answers")
+	}
+
+	if len(answerAfterSelectHooks) != 0 {
+		for _, obj := range resultSlice {
+			if err := obj.doAfterSelectHooks(ctx, e); err != nil {
+				return err
+			}
+		}
+	}
+	if singular {
+		object.R.Answers = resultSlice
+		for _, foreign := range resultSlice {
+			if foreign.R == nil {
+				foreign.R = &answerR{}
+			}
+			foreign.R.Choice = object
+		}
+		return nil
+	}
+
+	for _, foreign := range resultSlice {
+		for _, local := range slice {
+			if local.ID == foreign.ChoiceID {
+				local.R.Answers = append(local.R.Answers, foreign)
+				if foreign.R == nil {
+					foreign.R = &answerR{}
+				}
+				foreign.R.Choice = local
+				break
+			}
+		}
+	}
+
+	return nil
+}
+
 // SetQuestion of the choice to the related item.
 // Sets o.R.Question to related.
 // Adds o to related.R.Choices.
@@ -649,6 +738,59 @@ func (o *Choice) SetQuestion(ctx context.Context, exec boil.ContextExecutor, ins
 		related.R.Choices = append(related.R.Choices, o)
 	}
 
+	return nil
+}
+
+// AddAnswers adds the given related objects to the existing relationships
+// of the choice, optionally inserting them as new records.
+// Appends related to o.R.Answers.
+// Sets related.R.Choice appropriately.
+func (o *Choice) AddAnswers(ctx context.Context, exec boil.ContextExecutor, insert bool, related ...*Answer) error {
+	var err error
+	for _, rel := range related {
+		if insert {
+			rel.ChoiceID = o.ID
+			if err = rel.Insert(ctx, exec, boil.Infer()); err != nil {
+				return errors.Wrap(err, "failed to insert into foreign table")
+			}
+		} else {
+			updateQuery := fmt.Sprintf(
+				"UPDATE \"answers\" SET %s WHERE %s",
+				strmangle.SetParamNames("\"", "\"", 1, []string{"choice_id"}),
+				strmangle.WhereClause("\"", "\"", 2, answerPrimaryKeyColumns),
+			)
+			values := []interface{}{o.ID, rel.ID}
+
+			if boil.IsDebug(ctx) {
+				writer := boil.DebugWriterFrom(ctx)
+				fmt.Fprintln(writer, updateQuery)
+				fmt.Fprintln(writer, values)
+			}
+			if _, err = exec.ExecContext(ctx, updateQuery, values...); err != nil {
+				return errors.Wrap(err, "failed to update foreign table")
+			}
+
+			rel.ChoiceID = o.ID
+		}
+	}
+
+	if o.R == nil {
+		o.R = &choiceR{
+			Answers: related,
+		}
+	} else {
+		o.R.Answers = append(o.R.Answers, related...)
+	}
+
+	for _, rel := range related {
+		if rel.R == nil {
+			rel.R = &answerR{
+				Choice: o,
+			}
+		} else {
+			rel.R.Choice = o
+		}
+	}
 	return nil
 }
 
