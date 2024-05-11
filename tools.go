638b55c8d8785117d96183c8d6fe89cae0f3ce96
Sebastián Salata R-T <sa.salatart@gmail.com>
2024-05-11 19:47:19 -0400

    Setup React app with Vite

--- tools.go
+++ tools.go
@@ -5,4 +5,6 @@ package main
 import (
 	_ "github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen"
 	_ "github.com/dmarkham/enumer"
+	_ "github.com/volatiletech/sqlboiler/v4"
+	_ "github.com/volatiletech/sqlboiler/v4/drivers/sqlboiler-psql"
 )
