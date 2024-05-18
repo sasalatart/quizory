c4b59f53c95ec620bfd0f09d0115c9ffe0c2ae79
Sebastián Salata R-T <sa.salatart@gmail.com>
2024-05-18 18:19:35 -0400

    Configure react-router-dom (#37)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,40 @@
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
 import { QueryClientProvider, SessionProvider } from '@/providers';
-import { EnsureLoggedIn } from '@/screens/login';
-import { Question } from '@/screens/question';
-import { Layout } from './layout';
+import { Layout } from '@/layout';
+import { Login } from '@/screens/login';
+import { Question } from '@/screens/next-question';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <>
+        <SessionProvider>
+          <QueryClientProvider>
+            <Outlet />
+          </QueryClientProvider>
+        </SessionProvider>
+      </>
+    ),
+    children: [
+      {
+        path: '',
+        element: <Login />,
+      },
+      {
+        path: 'questions',
+        element: <Layout />,
+        children: [
+          {
+            path: 'next',
+            element: <Question />,
+          },
+        ],
+      },
+    ],
+  },
+]);
 
 export default function App() {
-  return (
-    <SessionProvider>
-      <QueryClientProvider>
-        <EnsureLoggedIn>
-          <Layout>
-            <Question />
-          </Layout>
-        </EnsureLoggedIn>
-      </QueryClientProvider>
-    </SessionProvider>
-  );
+  return <RouterProvider router={router} />;
 }
