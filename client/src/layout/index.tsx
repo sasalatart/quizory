d8e0a26a63259c1a21f02d086dd18d8e0d9cfb83
Sebastián Salata R-T <sa.salatart@gmail.com>
2024-09-24 21:16:07 -0300

    Add moving background to FE's layout (#97)

--- client/src/layout/index.tsx
+++ client/src/layout/index.tsx
@@ -1,4 +1,5 @@
-export { Layout } from './layout';
 export { CenteredSpinner, InlineSpinner } from './spinner';
+export { Layout } from './layout';
 export { Pagination } from './pagination';
 export { QuestionBadges } from './question-badges';
+export { WithSkybox } from './skybox';
---
+++ client/src/layout/skybox/index.tsx
@@ -0,0 +1,40 @@
+import { useRef } from 'react';
+import { Canvas, useFrame, useLoader } from '@react-three/fiber';
+import * as THREE from 'three';
+import skyboxImage from './skybox.jpg';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export function WithSkybox({ children }: Props): JSX.Element {
+  return (
+    <div className="h-screen">
+      <Canvas
+        className="absolute top-0 left-0 w-full h-full z-0"
+        camera={{ position: [0, 0, 0.1], fov: 75, near: 0.1 }}
+      >
+        <Skybox />
+      </Canvas>
+      <div className="absolute top-0 left-0 w-full h-full flex flex-col z-1">{children}</div>
+    </div>
+  );
+}
+
+function Skybox(): JSX.Element {
+  const sphereRef = useRef<THREE.Mesh>(null);
+  const texture = useLoader(THREE.TextureLoader, skyboxImage);
+
+  useFrame(() => {
+    if (sphereRef.current) {
+      sphereRef.current.rotation.y += 0.00033; // Adjust the speed as needed
+    }
+  });
+
+  return (
+    <mesh ref={sphereRef}>
+      <sphereGeometry args={[500, 60, 40]} />
+      <meshBasicMaterial map={texture} side={THREE.BackSide} />
+    </mesh>
+  );
+}
